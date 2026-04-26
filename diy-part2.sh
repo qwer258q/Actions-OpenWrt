629beb1a3875d36fffea394064f38148b9f813d6
qwer258q <100553605+qwer258q@users.noreply.github.com>
2026-04-26 22:29:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,34 +19,30 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-# 2. 彻底禁用/清理 small 源及官方 feeds 中的冲突插件
-# 这样做是为了确保系统“看不见”这些地方的 MosDNS
+# 2. 彻底清理 feeds 中的冲突项 (针对 small 源和官方源)
+# 清理 MosDNS 相关
 rm -rf feeds/small/mosdns
 rm -rf feeds/small/luci-app-mosdns
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
+
+# 清理 Netspeedtest 相关
+rm -rf feeds/small/netspeedtest
+rm -rf feeds/small/luci-app-netspeedtest
+rm -rf feeds/luci/applications/luci-app-netspeedtest
+
+# 清理 Geodata 相关
 rm -rf feeds/packages/net/v2ray-geodata
 
-# 3. 强制删除 package 目录下的旧目录（解决之前 Exit 128 的报错）
+# 3. 强制删除 package 目录下的残留，防止 git clone 报错 (Exit 128)
 rm -rf package/mosdns
 rm -rf package/v2ray-geodata
 rm -rf package/netspeedtest
 
-# --- 1. 物理删除：清理订阅源和 feeds 里的同名项 ---
-rm -rf feeds/small/luci-app-netspeedtest
-rm -rf feeds/small/netspeedtest
-rm -rf feeds/luci/applications/luci-app-netspeedtest
-rm -rf package/netspeedtest
-
-# 4. 重新下载你指定的精准源码
-# 这里下载的源码会存放在 package/ 目录下，优先级最高
+# 4. 重新拉取指定的精准源码
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/muink/luci-app-netspeedtest.git package/netspeedtest
-
-# 5. 额外建议：如果 mosdns 报错，通常是因为 golang 版本不对
-# 可以尝试屏蔽掉 feeds 里的 golang，强制编译使用最新版（可选）
-# rm -rf feeds/packages/lang/golang
 
 # 6. 额外建议：针对 24.10 的 Firewall4 优化
 # 确保在编译时默认开启一些网络加速特性
