b6e1d3896e569087ac120f928f53e6bac571b98b
qwer258q <100553605+qwer258q@users.noreply.github.com>
2026-04-26 20:36:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,18 +21,19 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 
 #!/bin/bash
 
-# 1. 替换 mosdns 和 v2ray-geodata (对应你图片中的逻辑)
-# 彻底清理旧的 feeds 冲突项
+# 清理 feeds 里的同名插件，确保 package 目录下的自定义插件生效
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
 
- # 拉取 sbwml 的 v5 分支版本
+# 先删除可能存在的旧插件目录，防止 clone 失败
+rm -rf package/mosdns
+rm -rf package/v2ray-geodata
+rm -rf package/netspeedtest
+
+# 然后再执行克隆
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-#luci-app-netspeedtest测速克隆
-rm -rf package/netspeedtest
 git clone https://github.com/muink/luci-app-netspeedtest.git package/netspeedtest
 
 # 6. 额外建议：针对 24.10 的 Firewall4 优化
