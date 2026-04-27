0e6f56a1593509cced3e624cd40214ae035a8523
qwer258q <100553605+qwer258q@users.noreply.github.com>
2026-04-27 09:13:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,30 +2,23 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
+# 1. 【核心修复】强制升级 Golang 环境到 1.24+
+# 这是解决 "requires go >= 1.24" 的根本办法
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 
-#!/bin/bash
+# 2. 【核心修复】彻底物理删除 small 源里的 v2dat (防止它干扰编译)
+# 因为 sbwml 的 mosdns 依赖会自动处理相关的数据文件，不需要 small 源里的旧包
+rm -rf feeds/small/v2dat
+rm -rf feeds/small/luci-app-mosdns
+rm -rf feeds/small/mosdns
 
-#--- 核心修复：升级 Go 环境 (解决 1.24 版本要求) ---
-
-rm -rf feeds/packages/lang/golanggit
-
-git clone https://github.com/sbwml/packages_lang_golang
- feeds/packages/lang/golang
-
-# 1. 修改默认 IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
-
-# 2. 暴力清理：不管有没有，先通通删一遍
-rm -rf feeds/small/mosdns feeds/small/luci-app-mosdns feeds/small/netspeedtest feeds/small/luci-app-netspeedtest
-rm -rf feeds/luci/applications/luci-app-mosdns feeds/packages/net/mosdns
-rm -rf feeds/luci/applications/luci-app-netspeedtest
-
-# 3. 物理抹除 package 目录 (解决 Exit 128 的核心)
+# 3. 清理残留的物理文件夹，防止 Exit 128
 rm -rf package/mosdns
 rm -rf package/v2ray-geodata
 rm -rf package/netspeedtest
 
-# 4. 重新拉取精准源码 (注意：每样只拉一次！)
+# 4. 重新拉取你指定的精准源码
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/muink/luci-app-netspeedtest.git package/netspeedtest
